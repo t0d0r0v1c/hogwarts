eb6a91f0ff7ef875ce0d2df5a46c7d9f85e86a15
t0d0r0v1c <jovantodorovic480@gmail.com>
2021-03-21 11:48:24 +0100

    files commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+function hogwarts(){
+    wp_enqueue_script('uvoz-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_style('uvoz-custom-uvoz-font', 'fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('uvoz-font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('hogwartsCSS', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'hogwarts');
+
+/**/
+function razna_cool_sranja() {
+    add_theme_support('title-tag'); // inserting title on the specific page trouht wp_head()
+    register_nav_menu('glavniMeni', 'Glavni Meni'); //dodavanje fjonalnosti da mozemo iz apperiance menus da pravimo meni
+    register_nav_menu('footerLocationOne', 'Futer lokacija jedan');
+    register_nav_menu('footerLocationTwo', 'Futer lokacija dva');
+}
+
+add_action('after_setup_theme', 'razna_cool_sranja'); //after_setup_theme je hook koji kaze da se okine ova akcija nakon sto se ucita tema
+
+?>
